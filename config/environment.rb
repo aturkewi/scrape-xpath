e620d6df7bb73abd52e1bf96a03dce23f8ebed71
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-10 14:24:32 -0500

    Add run file and pry

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative '../lib/adapters/groupon_connection.rb'
--- lib/adapters/groupon_connection.rb
+++ lib/adapters/groupon_connection.rb
@@ -1,3 +1,13 @@
 class GrouponConnection
 
+  attr_reader :connection
+
+  def initialize
+    @connection = self.class
+  end
+
+  def get_page(url)
+    Nokogiri::HTML(open(url))
+  end
+
 end
